b884db84960bd6738026b19cd79df3b4f1783689
TimBehl <timinaterb@gmail.com>
2016-10-20 20:51:17 -0600

    Updated screen, added base methods for building rooms

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ var stage;
 var queue;
 var titleScreen, gameScreen, instructionScreen, gameoverScreen, mapScreen;
 var menuButton, instructionButton, vsAIButton, vsPlayerButton;
+var blockSheet, blockSprite;
+var blockArray = [];
+var stageHeight = 600;
+var stageWidth = 800;
 
 manifest = [
     {src:"images/titlescreen.jpg", id:"title"},
@@ -13,6 +17,7 @@ manifest = [
     {src:"images/instructionbutton.png", id:"instructionbutton"},
     {src:"images/vsAIbutton.png", id:"vsAI"},
     {src:"images/vsplayerbutton.png", id:"vsPlayer"},
+    {src:"images/block.png", id:"block"},
     {src:"scripts/image_handler.js"},
     {src:"scripts/key_handler.js"},
     {src:"scripts/mouse_handler.js"},
@@ -40,14 +45,80 @@ function loadComplete(evt){
     vsPlayerButton = new createjs.Bitmap(queue.getResult("vsPlayer"));
     console.log("Everything loaded here boss!");
     
+    blockSheet = new createjs.SpriteSheet({
+        "frames": {
+            "width": 10,
+            "numFrames": 1,
+            "regX": 0,
+            "regY": 0,
+            "height": 10
+        },
+        "animations": {
+            "block": [0,0]
+        },
+        "images": [queue.getResult("block")]
+    });
+    blockSprite = new createjs.Sprite(blockSheet);
+    
     initMainLoop();
 }
 
 function setupCanvas() {
     var canvas = document.getElementById("game");
-    canvas.width = 800;
-    canvas.height = 600;
+    canvas.width = stageWidth;
+    canvas.height = stageHeight;
     stage = new createjs.Stage(canvas);
+}
+
+function setupMapOne(){
+    blockArray = [];
+    for(var i = 0; i < stageHeight; i += 10){
+        for(var j = 0; j < stageWidth; j += 10){
+            if((i === 0 || i === stageHeight-10) || (j === 0 || j === stageWidth-10)){
+                blockSprite.x = j;
+                blockSprite.y = i;
+                blockSprite.gotoAndStop("block");
+                blockArray.push(blockSprite.clone());
+            }
+        }
+    }
+    for(var k = 0; k < blockArray.length; k++){
+        stage.addChild(blockArray[k]);
+    }
+}
+
+function setupMapTwo(){
+    blockArray = [];
+    for(var i = 0; i < stageHeight; i += 10){
+        for(var j = 0; j < stageWidth; j += 10){
+            if((i === 0 || i === stageHeight-10) || (j === 0 || j === stageWidth-10)){
+                blockSprite.x = j;
+                blockSprite.y = i;
+                blockSprite.gotoAndStop("block");
+                blockArray.push(blockSprite.clone());
+            }
+        }
+    }
+    for(var k = 0; k < blockArray.length; k++){
+        stage.addChild(blockArray[k]);
+    }
+}
+
+function setupMapThree(){
+    blockArray = [];
+    for(var i = 0; i < stageHeight; i += 10){
+        for(var j = 0; j < stageWidth; j += 10){
+            if((i === 0 || i === stageHeight-10) || (j === 0 || j === stageWidth-10)){
+                blockSprite.x = j;
+                blockSprite.y = i;
+                blockSprite.gotoAndStop("block");
+                blockArray.push(blockSprite.clone());
+            }
+        }
+    }
+    for(var k = 0; k < blockArray.length; k++){
+        stage.addChild(blockArray[k]);
+    }
 }
 
 function gameLoop(){
